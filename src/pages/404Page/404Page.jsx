8d77c20e418e850523a4c3de680fa54f0ab9ff51
Nicolas Silva <nisiara@gmail.com>
2022-05-16 21:28:56 -0400

    Create 404 page and add route

---
+++ src/pages/404Page/404Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FourOhFourPage = () => {
+  return (
+    <div>404Page</div>
+  )
+}
+
+export default FourOhFourPage
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import LoginPage from '../pages/LoginPage/LoginPage'
 import SignUpPage from '../pages/SignUpPage/SignUpPage'
+import FourOhFourPage from '../pages/404Page/404Page'
 
 const routes = (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const routes = (
       <Route exact path='/' component={HomePage}/>
       <Route path='/login' component={LoginPage}/>
       <Route path='/sign-up' component={SignUpPage}/>
+      <Route path='*' component={FourOhFourPage} />
     </Switch>
   </BrowserRouter>
 )
